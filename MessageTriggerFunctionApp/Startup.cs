098cd6a61c3e14325f4f809bdefbd5a05f04d687
namanjain <naman.jain@nashtechglobal.com>
2023-11-20 12:29:27 +0530

    dependency injection done

---
+++ MessageTriggerFunctionApp/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataBaseHandling.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Services.IServices;
+using Services.Services;
+
+namespace MessageTriggerFunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddTransient<IStoargeQueueService, StoargeQueueService>();
+            builder.Services.AddTransient<ICosmoDbService,CosmoDbService>();
+            builder.Services.AddTransient<IEventGridService, EventGridService>();
+            builder.Services.AddLogging();
+        }
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            FunctionsHostBuilderContext context = builder.GetContext();
+
+            builder.ConfigurationBuilder
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: false)
+                .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: false)
+                .AddEnvironmentVariables();
+        }
+    }
+}
